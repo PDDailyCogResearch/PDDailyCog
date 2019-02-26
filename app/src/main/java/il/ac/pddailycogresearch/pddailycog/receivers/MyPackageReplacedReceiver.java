a15153ff7d1d67b663a232b739e8eceedce483f9
shanih <shna080@gmail.com>
2019-02-26 17:07:05 +0200

    Add dispatch sync job in update package receiver

--- app/src/main/java/il/ac/pddailycogresearch/pddailycog/receivers/MyPackageReplacedReceiver.java
+++ app/src/main/java/il/ac/pddailycogresearch/pddailycog/receivers/MyPackageReplacedReceiver.java
@@ -4,8 +4,15 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Trigger;
+
 import il.ac.pddailycogresearch.pddailycog.BuildConfig;
 import il.ac.pddailycogresearch.pddailycog.Firebase.FirebaseIO;
+import il.ac.pddailycogresearch.pddailycog.services.DataSyncJobService;
 import il.ac.pddailycogresearch.pddailycog.utils.CommonUtils;
 
 public class MyPackageReplacedReceiver extends BroadcastReceiver {
@@ -17,6 +24,23 @@ public class MyPackageReplacedReceiver extends BroadcastReceiver {
 //            FirebaseIO.getInstance().saveKeyValuePair("test", 1, "version-code", BuildConfig.VERSION_CODE);
 //        }
         //just triger the app class so data can be gethered
+
+        dispatcherSchedule(context); //TODO delete in next package update...
+    }
+
+    private void dispatcherSchedule(Context context) {
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
+
+        Job myJob = dispatcher.newJobBuilder()
+                .setService(DataSyncJobService.class) // the JobService that will be called
+                .setTag(DataSyncJobService.class.getSimpleName())
+                .setTrigger(Trigger.NOW)
+                .setConstraints(
+                        Constraint.ON_ANY_NETWORK
+                )
+                .build();
+
+        dispatcher.mustSchedule(myJob);
 
     }
 }
